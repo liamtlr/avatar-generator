6511866287cd25a7c7b6f516e0ed32932941699f
Liam Taylor <liam.taylor888@googlemail.com>
2017-07-30 14:36:57 +0100

    remves redundant tests  Please enter the commit message for your changes. Lines starting

--- spec/avatarGeneratorSpec.js
+++ spec/avatarGeneratorSpec.js
@@ -5,49 +5,54 @@ describe("AvatarGenerator", function() {
   var frame;
 
   beforeEach(function() {
-    avGen = new AvatarGenerator();
+    avGen = new AvatarGenerator(100, 50, 15, 10);
   });
 
   describe("Instantiation", function(){
-    it("receives an integer argument upon instatiation", function(){
-      var tester = new AvatarGenerator(3456)
-      expect(tester.visitorId).toEqual(3456);
+    it("receives an X value", function(){
+      expect(avGen.xValue).toEqual(100)
     });
-    // it("bowls again if you do not get a strike", function(){
-    //   spyOn(Math, 'random').and.returnValue(0.1);
-    //   frame.bowlFrame(game);
-    //   expect(frame.score.length).toEqual(2);
-    // });
-    // it("cannot have a frame totalling more than 10", function(){
-    //   frame.bowlFrame(game);
-    //   expect(frame.score[0]+frame.score[1]).toBeLessThan(11)
-    // });
+    it("receives a Y value", function(){
+      expect(avGen.yValue).toEqual(50)
+    });
+    it("receives a width value", function(){
+      expect(avGen.avatarWidth).toEqual(15)
+    });
+    it("receives a height value", function(){
+      expect(avGen.avatarHeight).toEqual(10)
+    });
   });
 
-  // describe("Final frame bonus:", function(){
-  //   beforeEach(function(){
-  //     for (var i = 0; i < 9; i++) {
-  //       frame.bowlFrame(game);
-  //       frame.resetFrame();
-  //       game.determineOutcomeofFrame();
-  //       game.calculateBonuses();
-  //     }
-  //     });
-  //   it("allows an extra bowl to calculate the bonus for the last frame if a spare is rolled", function() {
-  //     spyOn(Math, 'random').and.returnValues(0.5, 0.9, 0.3);
-  //     frame.bowlFrame(game);
-  //     frame.resetFrame();
-  //     game.determineOutcomeofFrame();
-  //     game.calculateBonuses();
-  //     expect(game.bonuses[game.bonuses.length-1]).toEqual([3])
-  //   });
-  //   it("allows an extra 2 bowls to calculate the bonus for the last frame if a strike is rolled", function() {
-  //     spyOn(Math, 'random').and.returnValues(1, 0.3, 0.3);
-  //     frame.bowlFrame(game);
-  //     frame.resetFrame();
-  //     game.determineOutcomeofFrame();
-  //     game.calculateBonuses();
-  //     expect(game.bonuses[game.bonuses.length-1]).toEqual([3, 3])
-  //   });
-  // });
+  describe("get co ordinates", function(){
+    it("returns an array of coordinates used to produce the avatar based on a visitor ID", function(){
+      expect(avGen.getCoordinates(5780)).toEqual([1380, 620]);
+    })
+  })
+
+  describe("hashing a visitorID", function(){
+    it("hashes the visitor ID for the x axis", function(){
+      expect(avGen.hashXCoordinate(450)).toEqual(61);
+    })
+    it("returns a value between one and the xValue", function(){
+      expect(avGen.hashXCoordinate(15677424)).not.toBeGreaterThan(avGen.xValue);
+    })
+    it("hashes the visitor ID for the y axis", function(){
+      expect(avGen.hashYCoordinate(260)).toEqual(56);
+    })
+    it("returns a value between one and the xValue", function(){
+      expect(avGen.hashYCoordinate(3645634532)).not.toBeGreaterThan(avGen.yValue);
+    })
+  });
+
+  describe("returning x coordinate", function(){
+    it("return the x co ordinate based on the avatar height and hashed value", function(){
+      expect(avGen.returnXCoordinate(50)).toEqual(750);
+    });
+  });
+
+  describe("returning the y co ordinate", function(){
+    it("returns the y co ordinate based on the hashed visitor ID", function(){
+      expect(avGen.returnYCoordinate(10)).toEqual(100);
+    })
+  })
 });
--- src/avatarGenerator.js
+++ src/avatarGenerator.js
@@ -1,3 +1,47 @@
-function AvatarGenerator(visitorId){
-  
-}
+function AvatarGenerator(xValue, yValue, avatarWidth, avatarHeight, reference){
+  this.xValue = xValue;
+  this.yValue = yValue;
+  this.avatarWidth = avatarWidth;
+  this.avatarHeight = avatarHeight;
+  this.spritesheet = reference;
+  this.randomContant = 19;
+};
+
+AvatarGenerator.prototype.hashCoordinate = function(visitorId, modulus){
+  var arr = visitorId.toString().split('')
+  var returnVal = 0;
+  for(var i=0;i<arr.length;i++){
+    returnVal = returnVal + (arr[i] ^ (this.randomContant + i));
+  };
+  return returnVal % modulus;
+  // return visitorId % this.xValue;
+  // return Math.floor(Math.random() * (this.xValue - 0 + 1)) + 0;
+};
+
+AvatarGenerator.prototype.getCoordinates = function(visitorId){
+  var hashedX = this.hashCoordinate(visitorId, this.xValue)
+  var reversedVisitorIdString = visitorId.toString().split('').reverse().join('');
+  var reversedVisitorId = parseInt(reversedVisitorIdString);
+  var hashedY = this.hashCoordinate(reversedVisitorId, this.yValue);
+  var xCoord = this.returnCoordinate(hashedX, this.avatarWidth);
+  var yCoord = this.returnCoordinate(hashedY, this.avatarHeight);
+  return [xCoord, yCoord];
+};
+
+AvatarGenerator.prototype.returnCoordinate = function(hashedvisitorId, dimension){
+  return hashedvisitorId * dimension;
+};
+
+AvatarGenerator.prototype.setAvatar = function(visitorId, htmlIdentifier){
+  var coOrds = this.getCoordinates(visitorId);
+  var ref =  "url('" + this.spritesheet + "')";
+  var objPos = coOrds[0] + "px " + coOrds[1] + "px";
+  $(document).ready(function(){
+      $(htmlIdentifier).css({
+          "height": this.avatarHeight,
+          "width": this.avatarWidth,
+          "background-image": ref,
+          "background-position": objPos
+      });
+  });
+};
